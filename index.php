cf13a37ecf85dfb8145c406126222cc1fe7ed7da
Sam Estrin <sam.estrin@gmail.com>
2024-04-23 13:41:56 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
 // Use the Composer-generated autoloader to handle necessary imports
 require_once __DIR__ . '/vendor/autoload.php';
