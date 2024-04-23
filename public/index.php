ad2f6dd3dde21e40ce541a53159214b0b6110054
Sam Estrin <sam.estrin@gmail.com>
2024-04-22 17:21:48 -0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
-if($_GET['debug'] == "true") { 
-    die("<pre>".print_r($_GET,true)."</pre>");
+// Check if the 'debug' query parameter is set and print the contents of $_GET for debugging purposes
+if ($_GET['debug'] ?? false === "true") {
+    die("<pre>" . print_r($_GET, true) . "</pre>");
 }
+
 // Use the Composer-generated autoloader to handle necessary imports
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -15,6 +17,4 @@ require_once __DIR__ . '/../src/bootstrap.php';
 require_once __DIR__ . '/../src/routes.php';
 
 // Any additional code required to handle the request can go here.
-
-
 ?>
